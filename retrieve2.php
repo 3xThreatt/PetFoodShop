5dea78c9718a7cf5ad8562dcee2b5360bc13229c
3xThreatt <74078818+3xThreatt@users.noreply.github.com>
2020-11-07 17:04:32 +1300

    Add files via upload

--- retrieve2.php
+++ retrieve2.php
@@ -2,7 +2,7 @@
 include("config.php");
 $sql2 = "select name from images where id=2";
 $result2 = mysqli_query($con,$sql2);
-$row2 = mysqli_fetch_array($result21);
+$row2 = mysqli_fetch_array($result2);
 
 $image2 = $row2['name'];
 $image_src2 = "uploads/".$image2;
