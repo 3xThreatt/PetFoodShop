b751653ce535da38a46979168fb4bb6597b7e581
3xThreatt <74078818+3xThreatt@users.noreply.github.com>
2020-11-07 16:54:37 +1300

    Add files via upload

--- retrieve.php
+++ retrieve.php
@@ -8,4 +8,19 @@ $row = mysqli_fetch_array($result);
 $image = $row['name'];
 $image_src = "uploads/".$image;
 
+$sql2 = "select name from images where id=2";
+$result2 = mysqli_query($con,$sql2);
+$row2 = mysqli_fetch_array($result21);
+
+$image2 = $row2['name'];
+$image_src2 = "uploads/".$image2;
+
+$sql3 = "select name from images where id=3";
+$result3 = mysqli_query($con,$sql2);
+$row3 = mysqli_fetch_array($result21);
+
+$image3 = $row3['name'];
+$image_src3 = "uploads/".$image3;
+
+
 ?>
