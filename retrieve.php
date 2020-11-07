97ad9add8b60b325d26c8b8b8d0b70dfaadfcc1e
3xThreatt <74078818+3xThreatt@users.noreply.github.com>
2020-11-07 17:05:22 +1300

    Add files via upload

---
+++ retrieve.php
@@ -0,0 +1,11 @@
+<?php
+include("config.php");
+
+$sql = "select name from images where id=1";
+$result = mysqli_query($con,$sql);
+$row = mysqli_fetch_array($result);
+
+$image = $row['name'];
+$image_src = "uploads/".$image;
+
+?>
