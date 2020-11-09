01e557cbea5ffd3b41653916633b33d92d248dd1
3xThreatt <74078818+3xThreatt@users.noreply.github.com>
2020-11-09 14:34:38 +1300

    Add files via upload

---
+++ updateQuantity.php
@@ -0,0 +1,19 @@
+<?php
+include("config.php");
+
+$sql = "select productQTY from Product where productID=1";
+$result = mysqli_query($con,$sql);
+$row = mysqli_fetch_array($result);
+
+$productQTY = $row['productQTY'];
+
+
+$quantityget = "UPDATE `Product` SET `productQTY` = '$productQTY'-'1' WHERE `Product`.`productID` = 1";
+$quantityresult = mysqli_query($con,$quantityget);
+$fetched = mysqli_fetch_array($quantityresult);
+
+$toprint = $fetched['productQTY']
+
+
+?>
+<p><?php echo $fetched ?></p>
