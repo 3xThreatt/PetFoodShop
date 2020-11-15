a1b674afa8ac0bb756f7dbde24f7ed90f742cdab
3xThreatt <74078818+3xThreatt@users.noreply.github.com>
2020-11-16 11:19:02 +1300

    Add files via upload

--- class5.php
+++ class5.php
@@ -15,5 +15,5 @@ $down = new Message();
 $down->set_error('The Login page is currently down for maintenance, please check in later');
 
 $up = new Message();
-$up->set_error('Thank you for waiting, the login page will be up shortly')
+$up->set_error('Thank you for waiting, the login page will be up shortly');
 ?>
