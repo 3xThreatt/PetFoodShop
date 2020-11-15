038a5ddf52304205a26381166c23198c235c7e3e
3xThreatt <74078818+3xThreatt@users.noreply.github.com>
2020-11-16 09:17:14 +1300

    Add files via upload

--- class2.php
+++ class2.php
@@ -16,6 +16,21 @@ class Display {
 $image = new Display();
 $image->set_image('menuimages/slideshow.png');
 
+$image2 = new Display();
+$image2->set_image('menuimages/slideshow1.png');
+
+$image3 = new Display();
+$image3->set_image('menuimages/slideshow2.png');
+
+$image4 = new Display();
+$image4->set_image('menuimages/slideshow3.png');
+
+$image5 = new Display();
+$image5->set_image('menuimages/slideshow4.png');
+
+$image6 = new Display();
+$image6->set_image('menuimages/slideshow5.png');
+
 
 
 ?>
