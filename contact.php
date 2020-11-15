0e50da350c3807509dbff06992141f03f5e6388e
3xThreatt <74078818+3xThreatt@users.noreply.github.com>
2020-11-16 10:35:59 +1300

    Add files via upload

--- contact.php
+++ contact.php
@@ -54,10 +54,32 @@ include("config.php");
          <div id="push">&nbsp;</div>
 
          <div <?php include("class4.php"); ?>> 
-            <?php echo $abel->get_name(); ?>       <?php echo $ben->get_name(); ?>  <br>
-            <?php echo $abel->get_position(); ?>     <?php echo $ben->get_position(); ?> <br>
-            <?php echo $abel->get_phone(); ?>         <?php echo $ben->get_phone(); ?> <br>
-            <?php echo $abel->get_email(); ?>         <?php echo $ben->get_email(); ?> <br>
+            <?php echo $abel->get_name(); ?><br>
+            <?php echo $abel->get_position(); ?><br>
+            <?php echo $abel->get_phone(); ?><br>
+            <?php echo $abel->get_email(); ?><br>
+                  <br>     <br>     <br>
+            <?php echo $ben->get_name(); ?>
+            <?php echo $ben->get_position(); ?>
+            <?php echo $ben->get_phone(); ?>
+            <?php echo $ben->get_email(); ?>
+                  <br>     <br>     <br>
+            <?php echo $harriot->get_name(); ?>
+            <?php echo $harriot->get_position(); ?>
+            <?php echo $harriot->get_phone(); ?>
+            <?php echo $harriot->get_email(); ?>
+                  <br>     <br>     <br>
+            <?php echo $peter->get_name(); ?>
+            <?php echo $peter->get_position(); ?>
+            <?php echo $peter->get_phone(); ?>
+            <?php echo $peter->get_email(); ?>
+
+
+
+
+
+
+
 
 
                <div id="push">&nbsp;</div>
